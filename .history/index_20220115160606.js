57acd1b35f0a62d117094969fec9e6d15987ee94
Rohit <rohitbharti@gmail.com>
2024-04-05 15:13:00 +0530

    first commit

---
+++ .history/index_20220115160606.js
@@ -0,0 +1,118 @@
+console.log("Weather app")
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".form-control");
+const api = "51d41d6f1c399b2abc7a3fcbfbb057c7";
+const weatherBox =document.querySelector(".weatherBox")
+let city = "";
+const arr =["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+btn.addEventListener("click", (e) => {
+    console.log(e);
+    e.preventDefault();
+
+    city = input.value;
+    console.log(city)
+    input.value = ""
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}`, true);
+
+    xhr.onload = function () {
+        
+        if (this.status == 200) {
+            let obj = JSON.parse(this.responseText);
+            console.log(obj.cod)
+            let ob = [];
+            // console.log(ob.push(...obj))/
+            let cardbox = document.querySelector(".cardBox");
+            let content = document.querySelector(".content")
+            str = "";
+            str2 = "";
+            let sunrise = displayEndtime(obj.sys.sunrise);
+            let sunset =displayEndtime(obj.sys.sunset);  
+            let date =displayDay();  
+            let c = `&#8451`;
+            if(obj.cod=="404")
+            {
+                   
+            }
+            else{
+            str += `
+            <div class="Box">
+                <div class="cardBox">
+                    <div class="cloudinfo">          
+                      <img src=${"./img/"}${obj.weather[0].main}${".png"}>
+                      <p>${obj.weather[0].main}${" | "}${"Feels like "}${(obj.main.feels_like-273).toFixed(0)}${c}</p>
+                    </div>
+                    <div class="name">
+                       <span>${obj.name}${" ,"}${obj.sys.country}</span> 
+                    </div>
+                    <div class="date">
+                       <p>${date}</p>
+                    </div>
+                    <div class="temp">
+                        <span class="tempNow">${(obj.main.temp -273.15).toFixed(0)}${c}</span>
+                        <div class="minmax">
+                           <span class="mintemp">${(obj.main.temp_min-273.15).toFixed(0)}${c}</span>
+                           <span class="maxtemp">${(obj.main.temp_max-273.15).toFixed(0)}${c}</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="weather">
+                        <span class="humidity"><img src="./img/blur.png"/>
+                           <p>${obj.main.humidity}${"%"}</p>
+                           <p>Humidity</p>
+                        </span>
+                        <span class="wind"><img src="./img/wind.png"/>
+                           <p>${obj.wind.speed}${"m/s"}</p>
+                           <p>Wind</p>
+                        </span>
+                        <span class="sunrise"> <img src="./img/sunrise.png"/>
+                           <p>${sunrise}</p>
+                           <p>Sunrise</p>
+                        </span>
+                    </div>
+                    <div class="location">
+                        <span class="lat">  <img src="./img/latitude.png"/>
+                          <p>${obj.coord.lat.toFixed(2)}</p>
+                          <p>Latitude</p>                  
+                        </span>
+                        <span class="lon"> <img src="./img/longitude.png"/>
+                          <p>${obj.coord.lon.toFixed(2)}</p><p>Longitude</p></span>
+                        <span class="sunrise"><img src="./img/sunset.png"/>
+                          <p>${sunset}</p>
+                          <p>Sunset</p>
+                        </span>
+                    </div>
+                </div>
+            </div>`;
+            weatherBox.innerHTML = str;
+    // console.log(str);
+            }
+        }
+        else
+        {
+            console.log("error")
+            str2+="City not found";
+            weatherBox.innerHTML=str2; 
+        }  
+        }
+    xhr.send();
+});
+
+function displayEndtime(tstamp) {
+    const end = new Date(tstamp);
+  
+    const hour = end.getHours();
+    const min = end.getMinutes();
+    const dis = `${hour > 12 ? hour - 12 : hour}:${min < 10 ? "0" : ""}${min}`;
+    return dis;
+}
+function displayDay() {
+    const end = new Date();
+  
+    const date = end.getDate();
+    const mon = end.getMonth();
+    const dis = `${arr[mon]}${","}${date}`;
+    return dis;
+}
+
